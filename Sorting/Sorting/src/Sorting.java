d41a9b055afcb0f1010655a92a76b9277fad09ea
Akansha Kumari <akanshak363@gmail.com>
2018-11-03 16:27:41 +0000

    Formatted

--- Sorting/Sorting/src/Sorting.java
+++ Sorting/Sorting/src/Sorting.java
@@ -53,25 +53,27 @@ public class Sorting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Sorting ob = new Sorting();
+		
 		Scanner s=new Scanner(System.in);
-        System.out.println("enter number of elements");
+        System.out.println("Enter number of elements:");
         int n=s.nextInt();
         int arr[]=new int[n];
-        System.out.println("enter elements");
-        for(int i=0;i<n;i++){//for reading array
+        System.out.println("\n\nEnter elements:");
+        
+        //for reading array
+        for(int i=0;i<n;i++){
             arr[i]=s.nextInt();
-            }
-		//int arr[] = {12,3,19,16,2,10,120,40,38};
+        }
 	    ob.bubbleSort(arr);
-	    System.out.println("The Bubble Sort");
+	    System.out.println("\n\nThe Bubble Sort:");
 	    ob.printArray(arr);
+	    
 	    ob.insertion(arr);
-	    System.out.println();
-	    System.out.println("The Insertion Sort");
+	    System.out.println("\n\nThe Insertion Sort:");
 	    ob.printArray(arr);
+	    
 	    ob.Selection(arr);
-	    System.out.println();
-	    System.out.println("The Selection Sort");
+	    System.out.println("\n\nThe Selection Sort:");
 	    ob.printArray(arr);
     } 
 		
