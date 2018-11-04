320018dbb18e169638bc8809e03cb7936715815e
Akansha Kumari <akanshak363@gmail.com>
2018-11-04 13:28:50 +0000

    Name updated

---
+++ Sorting/Sorting/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int l=0;
+		int r= 4;
+		int mid= (l+r)/2;
+		System.out.println(mid);
+	}
+
+}
